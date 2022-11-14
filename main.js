797a51de157df6a2c3a9c7c6a3c8ce8ba88221dc
Norma Cuevas <normapsc@hotmail.com>
2022-11-14 03:14:24 -0600

    Se agrego el componente del carrito de compras

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside= document.querySelector('.product-detail');
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 function toggleDesktopMenu () {
-    console.log('Click');
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
+
+    if (!isCarritoAsideClosed) {
+        aside.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu () {
-    console.log('Click');
+    const isCarritoAsideClosed = aside.classList.contains('inactive');
+
+    if (!isCarritoAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+
+function toggleCarritoAside () {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+
 }
